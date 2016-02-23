34d49a3af54072aa6a6629517058ef91e90d1c12
tterrag1098 <tterrag1098@gmail.com>
2016-02-23 04:01:23 -0500

    Add some comments to Heap.cpp

--- Lab3/Heap.cpp
+++ Lab3/Heap.cpp
@@ -19,14 +19,18 @@ Heap::~Heap()
 
 void Heap::insert(int i)
 {
+    // First make sure there's room
     if (size >= 100)
     {
         cout << "Heap is full!" << endl;
         return;
     }
 
+    // Add the new element to the next slot
     data[size] = i;
+    // Increase the size to include the new element
     size++;
+    // Start at the parent of the new element (which is now at size - 1), and iterate up the parents, heapifying each.
     for (int idx = parent(size - 1); idx >= 0; idx = parent(idx))
     {
         heapify(idx);
@@ -41,13 +45,17 @@ int Heap::getMax()
 int Heap::extractMax()
 {
     int ret = getMax();
+    // Set the head to the lowest possible value
     data[0] = INT_MIN;
+    // Heapify the head, bringing the new highest value to the top
     heapify(0);
+    // Iterate and replace the dummy element with the last element in the heap
     for (int i = 0; i < size; i++)
     {
         if (data[i] == INT_MIN)
         {
             data[i] = data[size - 1];
+            data[size - 1] = 0;
         }
     }
     size--;
@@ -108,15 +116,18 @@ void Heap::heapify(int i)
 {
     int l = left(i), r = right(i);
     int large = i;
+    // If the left child is in the heap, check if it is larger
     if (l < size && data[l] > data[large])
     {
         large = l;
     }
+    // Next, do the same for the right child
     if (r < size && data[r] > data[large])
     {
         large = r;
     }
 
+    // If a child was larger, swap it with the parent and then heapify that child
     if (large != i)
     {
         int temp = data[i];
@@ -128,15 +139,20 @@ void Heap::heapify(int i)
 
 int* Heap::sort()
 {
+    // Cache the size, as it will be modified by the sort algorithm
     int old_size = size;
     for (int i = size - 1; i > 0; i--)
     {
+        // Swap the biggest element with the last element
         int temp = data[0];
         data[0] = data[i];
         data[i] = temp;
+        // "Cut off" the last element (which is currently the highest)
         size--;
+        // Heapify the new root element to restore the heap property
         heapify(0);
     }
+    // Restore the size so that it can be referenced by caller
     size = old_size;
     return data;
 }
